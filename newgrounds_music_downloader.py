8b1928fd5464f320d5f474f7b63e57f13dcaf84a
JackLSummer15 <62530870+JackLSummer15@users.noreply.github.com>
2021-06-16 21:19:31 -0400

    new version

---
+++ newgrounds_music_downloader.py
@@ -0,0 +1,14 @@
+import os
+
+print('NEWGROUNDS MUSIC DOWNLOADER BY: JACKLSUMMER15')
+
+#get song here
+songid=input('Enter song ID: ')
+
+#link
+mp3='https://www.newgrounds.com/audio/download/'+songid
+
+#downloading process
+os.system('curl --fail --silent -# -H "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L '+mp3+' -o music/'+songid+'.mp3')
+
+
